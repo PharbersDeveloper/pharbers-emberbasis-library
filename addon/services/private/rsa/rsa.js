33abe588694456dc03fcca0533529d72fe3658ea
Faiz <891366402@qq.com>
2018-10-24 00:04:10 +0800

    RSA分段加密前端可以根据公钥私钥进行加密解密，后端待测试

--- addon/services/private/rsa/rsa.js
+++ addon/services/private/rsa/rsa.js
@@ -4,31 +4,64 @@ import Base from './rsa-base';
 export default Service.extend(Base, {
     init() {
         this._super(...arguments);
-        this.initInstance({ b: 1024 });
+        this.initInstance();
     },
+    publicKey: "",
+    privateKey: "",
+
     /**
-     * 设置公钥到实例中
-     * @param {导入公钥} key 
+     * 设置公钥
+     * @param {String} publickey 
      */
-    importKey(key, format) {
-        this.get('Instance').importKey(key, format);
+    setPublicKey(publickey) {
+        this.set('publicKey',
+            this.get('RSAInstance').setPublicKey(publickey)
+        )
     },
 
     /**
-     * 加密
-     * @param {内容} content 
-     * @param {加密类型：`base64`} type 
+     * 设置私钥
+     * @param {String} privatekey 
      */
-    encrypt(content, type) {
-        return this.get('Instance').encrypt(content, type)
+    setPrivateKey(privatekey) {
+        this.set('privateKey',
+            this.get('RSAInstance').setPrivateKey(privatekey)
+        )
     },
 
     /**
-     * 解密
-     * @param {内容} content 
-     * @param {解密类型：`utf-8`} type 
+     * 内容加密 (短)
+     * @param {String} content 
+     * @returns {String} 密文
      */
-    decrypt(content, type) {
-        return this.get('Instance').decrypt(content, type)
+    encrypt(content) {
+        return this.get('RSAInstance').encrypt(content);
+    },
+
+    /**
+     * 加密内容（长）
+     * @param {String} content 
+     * @returns {String} 密文
+     */
+    encryptLong(content) {
+        return this.encryptLong2(content)
+    },
+
+    /**
+     * 内容解密（长）
+     * @param {String} encryptContent 
+     * @returns {String} 解密
+     */
+    decryptLong(encryptContent) {
+        return this.decryptLong2(encryptContent)
+    },
+
+    /**
+     * 内容解密 (短)
+     * @param {String} encryptContent 
+     * @returns {String} 解密
+     */
+    decrypt(encryptContent) {
+        return this.get('RSAInstance').decrypt(encryptContent)
     }
 });
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,30 +1,44 @@
 import Controller from '@ember/controller';
 import { inject } from '@ember/service';
-import { JSEncrypt} from 'jsencrypt';
 
 export default Controller.extend({
     phservice: inject(),
     init() {
         this._super(...arguments);
-
-        // this.NodeRSA()
         this.JSRSA()
+        // this.BusinessLogic()
 
     },
     JSRSA() {
-        // this.get('logger').log(JSEncrypt);
-        let encrypt = new JSEncrypt();
-        // let pubkey = "-----BEGIN PUBLIC KEY-----MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQAB-----END PUBLIC KEY-----"
-        let pubkey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAMShq/Oe/YL0/XA//N70oXmCVHDTeKfdIVJPEYSQ2pcxCm9GZ9ysZVgNkdAei7tpLFRhjmyeexX+0U8Js0AWn80CAwEAAQ=="
-        encrypt.setPublicKey(pubkey);
-        let encrypted = encrypt.encrypt('你好RSA');
-        this.get('logger').log(encrypted);
+        let PublicKey = `MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAK9L5z4lVKzUdffDqndr78I+ovAZ9W/AOTf9AYqOWgw1ZzJuHSHZL8iCtkfSR9KMsLC/wxsNHigUFyKsaTWFIi8CAwEAAQ==`;
+        let PrivateKey = `MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAr0vnPiVUrNR198Oqd2vvwj6i8Bn1b8A5N/0Bio5aDDVnMm4dIdkvyIK2R9JH0oywsL/DGw0eKBQXIqxpNYUiLwIDAQABAkAex1ID3GQgsHFCHo3ox//h+EN9quEoTPT++qJxpIr1B4T6DAUOzuI68/eZy5MGpUvi4vhtmYn9mrbVeqZnrcDBAiEA2hUHPCoDjSibsp1o5IMhgIXlnKsz7+9UgC+FW6vDgOECIQDNxnWaq53Q27LmFP6mM/sHDN2uMx0nvphKhqQ9vaI1DwIhANIHgIFMAUGYg2LhQJ0bQU+zJLDfHVUNzPbrTWc9JDthAiAHlSuaQn6zRpVGEzn7B+lVLi0xESMe5tAX1vRQbh9/EwIgf2TdQUtdoCDGHQA6NrEZ8jqMX4opbwa8WqqNewuBXPw=`;
+        let RSA = this.get('phservice').get('RSA');
+        RSA.setPublicKey(PublicKey)
+        
+        // let encryptString = RSA.encrypt("富强、民主、文明、和谐、自由、平等、公正、法治、爱国、敬业、诚信、友善")
+        // const params = {
+        //     'cash_amount_to':1,
+        //     'pick_all': 'false',
+        //     'withdraw_type':'CHANNEL_EMONEY',
+        //     'third_account_channel': '大萨达撒撒',
+        //     'third_account': '15900000000',
+        //     'third_account_name':'name',
+        //     'client_ip':'127.0.0.1',
+        //     'password':'ed40beecde2036df41a6a7c907fee1'
+        // };
+        // let encryptString = RSA.encryptLong(JSON.stringify(params));
+        let encryptString = RSA.encryptLong("富强、民主、文明、和谐、自由、平等、公正、法治、爱国、敬业、诚信、友善,{},\ndasdasdkjnfdsljk\ndasda\t");
+        this.get('logger').log(encryptString);
 
-        // let prikey = "-----BEGIN RSA PRIVATE KEY-----MIICXQIBAAKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQABAoGAfY9LpnuWK5Bs50UVep5c93SJdUi82u7yMx4iHFMc/Z2hfenfYEzu+57fI4fvxTQ//5DbzRR/XKb8ulNv6+CHyPF31xk7YOBfkGI8qjLoq06V+FyBfDSwL8KbLyeHm7KUZnLNQbk8yGLzB3iYKkRHlmUanQGaNMIJziWOkN+N9dECQQD0ONYRNZeuM8zd8XJTSdcIX4a3gy3GGCJxOzv16XHxD03GW6UNLmfPwenKu+cdrQeaqEixrCejXdAFz/7+BSMpAkEA8EaSOeP5Xr3ZrbiKzi6TGMwHMvC7HdJxaBJbVRfApFrE0/mPwmP5rN7QwjrMY+0+AbXcm8mRQyQ1+IGEembsdwJBAN6az8Rv7QnD/YBvi52POIlRSSIMV7SwWvSK4WSMnGb1ZBbhgdg57DXaspcwHsFV7hByQ5BvMtIduHcT14ECfcECQATeaTgjFnqE/lQ22Rk0eGaYO80cc643BXVGafNfd9fcvwBMnk0iGX0XRsOozVt5AzilpsLBYuApa66NcVHJpCECQQDTjI2AQhFc1yRnCU/YgDnSpJVm1nASoRUnU8Jfm3Ozuku7JUXcVpt08DFSceCEX9unCuMcT72rAQlLpdZir876-----END RSA PRIVATE KEY-----"
-        let prikey = "MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAxKGr8579gvT9cD/83vSheYJUcNN4p90hUk8RhJDalzEKb0Zn3KxlWA2R0B6Lu2ksVGGObJ57Ff7RTwmzQBafzQIDAQABAkAoYNLAuntwswKAVIOG/VFTdHIGoUZ7ois/jcEZKe3ZcfpxRVjcbsMwaZoY+hYRIbjHQJW+ybkmPhgwWjWyLTl5AiEA6DGj8cbsHTkOml00iQcGe0Yir2u5JhlaQvpZciIsfS8CIQDYyqF4ODn0XP476ub8NsRt2dUqRFvD2TzqRn2KWEHLwwIhAJ9zP19QOGLVBNcyfFpBLc1H25fVhP7J2Dc4jmYXW6/XAiAeOKEp0SzDE/OdBrIMltXrOmKs8bqZIr+LKSh3ELabYQIgAU4feMPdRH8ot/T8esKN+6kI0Z/JEuavk8PgMbRasPU="
-        let decrypt = new JSEncrypt();
-        decrypt.setPrivateKey(prikey);
-        var uncrypted = decrypt.decrypt(encrypted);
-        this.get('logger').log(uncrypted);
+        RSA.setPrivateKey(PrivateKey);
+        let uncrypted = RSA.decryptLong(encryptString)
+        // let uncrypted = RSA.decryptLong("c7T4aeSyrNo7PCZFlEeOXGzj3hQz2S/h+nm9uCEVesckWmVchgQF9h9gb3UP2LRvtbkATDrFZ7PmGZXyHf2JE2Vq8doK1qLAf7cij6Wu0hMfY5CovjWu09rnmpcoj649Ajh5wkyxInMIWwkyZ9GSSgmb+c95J4rrvtIvJbFiG/gypJcNfTNzH53NH8pViklW7WJJVDdxWey7DIH8drDyRw2cdE0SAsmsTY5VklHVBd2DRqsK5PlWovWuIGjzSazV");
+        this.get('logger').log(uncrypted); 
+    },
+    BusinessLogic() {
+        const loginFunc = function() {
+            this.get('logger').log('=======> loginFunc');
+        }
+        this.get('phservice').get('BusinessLogic').funcInjection(loginFunc)
     }
 });
